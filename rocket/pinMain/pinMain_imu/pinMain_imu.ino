8f5d20d300ef2f5d1176709bcf6eb798e974d93e
chanyeong <azaz999977@gmail.com>
2026-07-25 00:26:56 +0900

    중립각 수정

--- rocket/pinMain/pinMain_imu/pinMain_imu.ino
+++ rocket/pinMain/pinMain_imu/pinMain_imu.ino
@@ -50,8 +50,8 @@ static const uint8_t  MOTOR_CH2   = 15;
 static const uint16_t SERVO_MIN_US = 500;
 static const uint16_t SERVO_MAX_US = 2500;
 
-static const float   SERVO_NEUTRAL_DEG1 = 79.0f;  //a급은 79.0
-static const float   SERVO_NEUTRAL_DEG2 = 79.0f;  //a급은 79.0
+static const float   SERVO_NEUTRAL_DEG1 = 100.0f;  //a급은 100.0, b급은79.0
+static const float   SERVO_NEUTRAL_DEG2 = 100.0f;  //a급은 100.0, b급은79.0
 static float servoDeg1 = SERVO_NEUTRAL_DEG1;
 static float servoDeg2 = SERVO_NEUTRAL_DEG2;
 // [설정] 서보 물리적 제한 각도
